4c258ff3fdf0d1c1ad86084ea0d7166a028e4f9a
Jeremiah Ikechukwu Egbune <jegbune1@gmail.com>
2023-04-09 00:10:00 +0100

    React way of programming declaratively

--- index1.js
+++ index1.js
@@ -1,4 +1,7 @@
-const main = document.getElementById("root");
-const h1 =document.createElement("h1");
-h1.textContent ="This is an imperative way to program";
-main.appendChild(h1)
+// const main = document.getElementById("root");
+// const h1 =document.createElement("h1");
+// h1.textContent ="This is an imperative way to program";
+// h1.className = "header"
+// main.appendChild(h1)
+
+ReactDOM.render(<h1>This is a declarative way to program</h1>, document.querySelector("#root "))
