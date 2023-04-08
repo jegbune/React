d6f3ba4de8097e873adcb3641190d2d37e7df17b
Jeremiah Ikechukwu Egbune <jegbune1@gmail.com>
2023-04-08 23:59:15 +0100

    React exercise on imperative way to program

--- index1.js
+++ index1.js
@@ -1,5 +1,4 @@
-const main = document.getElementById("root")
-
+const main = document.getElementById("root");
 const h1 =document.createElement("h1");
 h1.textContent ="This is an imperative way to program";
-main.append(h1)
+main.appendChild(h1)
