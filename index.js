e45b20b8b7e3a9d59fe5d9be9850fe8cb289e4ee
Jeremiah Ikechukwu Egbune <jegbune1@gmail.com>
2023-04-08 01:27:55 +0100

    React Exercise for beginner

--- index.js
+++ index.js
@@ -25,10 +25,36 @@ function Navbar() {
                         <a className = "nav-link dropdown-toggle" href = "#" id = "navbarDropdown" role = "button" data-toggle = "dropdown" aria-haspopup = "true" aria-expanded = "false">
                             dropdown
                         </a>
-                        
+                        <div className = "dropdown-menu" aria-labelledby = "navbarDropdown">
+                            <a className = "dropdown-item" href = "#">Action</a>
+                            <a className = "dropdown-item" href = "#"> Another action</a>
+                            <div className = "dropdown-divider"></div>
+                            <a className= "dropdown-item" href = "#"> Something else here</a>
+                        </div>
+
+                    </li>
+                    <li className = "nav-item">
+                        <a className = "nav-link disabled" href = "#"> Disabled</a>
                     </li>
                 </ul>
+                <form className = "form-inline ny-2 ny-lg-0">
+                    <input className = "form-control my-sm-2" type = "search" placeholder = "Search" aria-label = "Search" />
+                    <button className = "btn btn-outline-success ny-2 ny-sm-0" type = "submit">Search</button>
+                </form>
             </div>
         </nav>
     )
 }
+
+function MainContent() {
+    return (
+        <h1>I am learning React</h1>
+    )
+}
+ReactDOM.render(
+    <div>
+        <Navbar />
+        <MainContent />
+    </div>, 
+    document.getElementById("root")
+)
---
+++ index1.js
@@ -0,0 +1,5 @@
+let main = document.getElementById("root")
+
+let h1 =document.createElement("h1");
+h1.textContent ="Hello, React!";
+main.appendChild(h1)
